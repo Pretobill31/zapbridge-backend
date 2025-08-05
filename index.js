978d5c64857781274cfe2102f0ad9255df73a350
Pretobill31 <billalves907@gmail.com>
2025-08-05 15:00:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import makeWASocket, { useMultiFileAuthState } from "@adiwajshing/baileys";
+import makeWASocket, { useMultiFileAuthState } from "@whiskeysockets/baileys";
 import QRCode from "qrcode";
 
 const app = express();
@@ -7,24 +7,27 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/generate-qr", async (req, res) => {
   try {
-    const { state, saveCreds } = await useMultiFileAuthState("./sessions");
+    const { state, saveCreds } = await useMultiFileAuthState("sessions");
 
     const sock = makeWASocket({
       auth: state,
-      printQRInTerminal: true,
+      printQRInTerminal: false,
     });
+
+    let sent = false;
 
     sock.ev.on("connection.update", async (update) => {
       const { qr } = update;
-      if (qr) {
+      if (qr && !sent) {
         const qrImage = await QRCode.toDataURL(qr);
         res.json({ qrCode: qrImage });
+        sent = true;
       }
     });
 
     sock.ev.on("creds.update", saveCreds);
   } catch (err) {
-    console.error("Erro ao gerar QR Code:", err);
+    console.error("❌ Erro ao gerar QR Code:", err);
     res.status(500).json({ error: "Erro ao gerar QR Code" });
   }
 });
